00ffb4a19357f05968fb3d128232e9f148155bb6
Joris Wijlens <joris@wijlens.com>
2010-08-26 23:46:57 +0200

    Made test more robust

--- src/test/scala/VoorwerpenRepositoryTest.scala
+++ src/test/scala/VoorwerpenRepositoryTest.scala
@@ -2,12 +2,13 @@ package com.wijlens.ibgv
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
-import org.testng.annotations.Test
 import org.testng.Assert
 import javax.persistence.EntityManagerFactory
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.test.context.transaction.TransactionConfiguration
 import org.springframework.test.context.testng.{AbstractTransactionalTestNGSpringContextTests}
+import org.springframework.test.annotation.Rollback
+import org.testng.annotations.{AfterMethod, Test}
 
 
 /**
@@ -18,7 +19,6 @@ import org.springframework.test.context.testng.{AbstractTransactionalTestNGSprin
  *
  */
 @ContextConfiguration(locations = Array("classpath:/applicationContext.xml"))
-@Transactional
 class VoorwerpenRepositoryTest extends AbstractTransactionalTestNGSpringContextTests{
 
   @Autowired var voorwerpenRepository: VoorwerpenRepository = null
@@ -27,9 +27,11 @@ class VoorwerpenRepositoryTest extends AbstractTransactionalTestNGSpringContextT
   def vindEnCreeerVoorwerpen {
     Assert.assertEquals(voorwerpenRepository.findAll().size,0)
     voorwerpenRepository.save(InBeslagGenomenVoorwerp("1234"))
-    voorwerpenRepository.flush
     val voorwerpen = voorwerpenRepository.findAll()
     Assert.assertEquals(voorwerpen size,1)
     Assert.assertTrue(voorwerpen.exists(_.code == "1234"))
   }
+
+  @AfterMethod
+  def after ={ deleteFromTables("in_beslag_genomen_voorwerpen")}
 }
